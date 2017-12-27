c114b67764bca1fc40d69e94f63e064e7a772d02
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2017-12-27 00:03:13 -0500

    factorial

---
+++ factorial/factorial.s
@@ -0,0 +1,32 @@
+# PURPOSE: Perform a factorial
+
+.section .data
+.section .text
+.globl _start
+.globl _factorial # Do this to allow sharing
+
+_start:
+    pushl $4
+    call factorial
+    addl $4, %esp
+    movl %eax, %ebx
+    movl $1 , %eax
+    int $0x80
+
+factorial:
+    pushl %ebp
+    movl %esp, %ebp
+    # 4(%ebp) holds the just pushed return addres, while 8(%ebp) holds the value 
+    movl 8(%ebp), %eax
+    cmpl $1, %eax
+    je end_factorial
+    decl %eax
+    pushl %eax
+    call factorial
+    movl 8(%ebp), %ebx
+    imul %ebx, %eax
+
+end_factorial:
+    movl %ebp, %esp
+    popl %ebp
+    ret
